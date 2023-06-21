6a429c35faba658d921e2d9ad216a662a26ec5b2
miguelcl <miguel221986@gmail.com>
2023-06-20 22:51:06 -0300

    segunda preEntrega

--- js/main.js
+++ js/main.js
@@ -10,10 +10,20 @@ const TEXTPASSERROR2 = "La contraseña no debe tener espacios.\nIngrese \"3\" pa
 const TEXTPASSERROR3 = "¡Debe escribir una contraseña!\nIngrese \"3\" para volver al menú principal.\n"
 const TEXTPASSERROR4 = "Las contraseñas no coinciden.\nIngrese \"3\" para volver al menú principal.\n"
 const TEXTMENUCALCULATOR = ""
+const TEXTMENUCOMBINATION = "Elija una opción:\n\n 1 - Listar las combinaciones\n 2 - Buscar combinaciones a través de un elemento\n 3- Buscar combinaciones a través de su ID (la ID va del 1 hasta el total de combinaciones)\n 4 - Agregar una combinación a mi colección\n 5 - Listar mi colección\n 6 - Listar mi colección en orden ascendente o descendente\n 7 - Borrar una combinación de la colección\n 8 - Salir"
 
 let user
 let password
 
+const combinationList = []
+const combinationCollection = []
+
+class Listcombination {
+    constructor(id, combination) {
+        this.id = id
+        this.combination = combination
+    }
+}
 menuStart(text(1))
 alert("Fin del programa")
 
@@ -43,6 +53,8 @@ function text(option) {
             return TEXTLOGINUSER
         case 12: 
             return TEXTMENUCALCULATOR  
+        case 13:
+            return TEXTMENUCOMBINATION
     }
 }
 function menuStart(option) {
@@ -84,7 +96,161 @@ function menuStart(option) {
                 break
             }
     }
+}
+function selection(array, propertie, value, operation) {
+    if (operation === "includes") {
+        return array.filter(element => element[propertie].includes(value))
     }
+    else if (operation === "===") {
+        let a = array.find(element => element[propertie] === value)
+        return a
+    }
+}
+function toList(array) {
+    let lista = "ID - Combinación\n"
+    for (const elemento of array) {
+        lista = lista + elemento.id + " - " + elemento.combination + "\n"
+    }
+    return lista
+}
+function toListArray(array) {
+    let lista = "Resultado:\n"
+    for (const elemento of array) {
+        lista = lista + elemento.combination + "\n"
+    }
+    return lista
+}
+function menuCombination(option, n, a, b) {
+    let menu 
+    while (menu !== 8 ) {
+        menu = Number(prompt(option))
+        switch (menu) {
+            case 1:
+                let messageList = toList(combinationList)
+                alert(messageList)
+                break
+            case 2:
+                let search = Number(prompt("Ingrese un elemento que desea encontrar en la combinación"))
+                let  messageSearch = selection(combinationList,"combination", search, "includes")
+                alert(toListArray(messageSearch))
+                break
+            case 3:
+                let id = Number(prompt("Ingrese un ID. Puede ser un valor de 1 a " + n))
+                let messageId = selection(combinationList, "id", id, "===")
+                alert(messageId.combination)
+                break
+            case 4:
+                let addCombination = Number(prompt("Ingrese un ID de alguna combinación para agregar a su colección"))
+                let extractCombination = selection(combinationList, "id", addCombination, "===")
+                if (extractCombination !== undefined) {
+                    combinationCollection.push(extractCombination)
+                }
+                else {
+                    alert("ID incorrecto!")
+                }
+                break
+            case 5:
+                if (combinationCollection.length !== 0) {
+                    let message = toList(combinationCollection)
+                    alert(message)
+                }
+                else {
+                    alert("La colección está vacía")
+                }
+                break
+            case 6:
+                if (combinationCollection.length !== 0) {
+                    let collectionOrderOption = Number(prompt("Ingrese \"1\" para ordenar ascendente o \"2\" para ordenar descendente"))
+                    if (collectionOrderOption === 1) {
+                        combinationCollection.sort((a, b) => a.id-b.id)
+                        let message = toList(combinationCollection)
+                        alert(message)
+                    }
+                    else if (collectionOrderOption === 2) {
+                        combinationCollection.sort((a, b) => b.id-a.id)
+                        let message = toList(combinationCollection)
+                        alert(message)
+                    }
+                    else {
+                        alert("Opción incorrecta")
+                        return
+                    }
+                }
+                else {
+                    alert("La colección está vacía")
+                }
+                break
+                case 7:
+                    if (combinationCollection.length !== 0) {
+                        combinationCollection.sort((a, b) => a.id-b.id)
+                        let messageListSup = toList(combinationCollection)
+                        let supCombination = Number(prompt("Ingrese el ID que quiera borrar\n" + messageListSup))  
+                        let supCombinationSearch = combinationCollection.find(element => element.id === supCombination)
+                        if (supCombinationSearch) {
+                            let au = combinationCollection.indexOf(supCombinationSearch)
+                            combinationCollection.splice(au, au+1)
+                            alert("Se eliminó correctamente la combinación")
+                        }
+                        else {
+                            alert("El ID es incorrecto")
+                        }
+                    }
+                    else {
+                        alert("La colección está vacía")
+                    }
+                    break
+                    
+            default:
+                break
+        }
+    }
+}
+function combinationGenerator(n, k) {
+    let result = []
+    let aux = [k]
+    let j = 0
+    let i = 1
+    generator(j , i, k)
+    return result
+    function generator(j , i, k) {
+        for (; i <= n; i++) {
+            aux[j] = i
+            if (j === k - 1) {
+                result.push(aux.slice())    
+            }
+            else {
+                j++
+                generator(j, i + 1, k)
+            }
+            if (aux[j] === n) {
+                j--
+            }
+        }
+    }
+}
+function combinationGeneratorRep(n, k) {
+    let result = []
+    let aux = [k]
+    let j = 0
+    let i = 1
+    generator(j , i, k)
+    return result
+    function generator(j , i, k) {
+        for (; i <= n; i++) {
+            aux[j] = i
+            if (j === k - 1) {
+                result.push(aux.slice())    
+            }
+            else {
+                j++
+                generator(j, i, k)
+            }
+            if (aux[j] === n) {
+                j--
+            }
+        }
+    }
+}
 function calculator() {
     let start = "CALCULADORA DE COMBINACIONES.\n\n\nElija una opción:\n\n1 - Calculadora de combinaciones sin repetición.\n2- Calculadora de combinaciones con repeticiones.\n3 - Salir.\n"
     let combinacionesSinRepeticiones = "Calculadora de combinaciones sin repetición.\n\nFórmula: \u207FC\u2096 = n! / k!(n-k)!\n\nDónde n es el número de elementos a escoger y k el número de elementos de cada combinación.\n\nRecuerde que n debe de ser mayor igual que k para que existan combinaciones\n\n"
@@ -106,6 +272,8 @@ function calculator() {
                 do {
                     k = Number(prompt(combinacionesSinRepeticiones + "Ingrese un valor válido de k, por favor"))
                 } while (isNaN(k) || k <= 0)
+                let a = n
+                let b = k
                 let l = k - 1
                 let j = n - 1
                 for(let i = k-1 ; i > 0; i-- && j-- && l--) {
@@ -114,6 +282,12 @@ function calculator() {
                 }
                 n = n/k
                 alert("Existen "+ n + " de combinaciones.")
+                const num = combinationGenerator(a, b)
+                
+                for (let i = 0, j = 1; i < num.length; i++, j++) {
+                    combinationList[i] = new Listcombination(j, num[i])
+                }
+                menuCombination(text(13), n, a, b)
                 break
             case 2:
                 let x
@@ -124,6 +298,8 @@ function calculator() {
                 do {
                     y = Number(prompt(combinacionesConRepeticiones + "Ingrese un valor válido de k, por favor"))
                 } while (isNaN(y) || y <= 0)
+                let r = x
+                let t = y
                 let e = (x + (y-1))
                 let s = x-1
                 for(let i = e-1; i >=x ; i--) {
@@ -138,6 +314,12 @@ function calculator() {
                     break
                 }
                 alert("Existen "+ x + " de combinaciones.")
+                const number = combinationGeneratorRep(r, t)
+                
+                for (let i = 0, j = 1; i < number.length; i++, j++) {
+                    combinationList[i] = new Listcombination(j, number[i])
+                }
+                menuCombination(text(13), x, r, t)
                 break
             default:
                 
